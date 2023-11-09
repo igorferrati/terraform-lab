7573677c0eacae7142f77655cb8da89f3ea0f651
igorferrati <ferrati.igor@gmail.com>
2023-11-08 22:35:57 -0300

    add security list

---
+++ oci/ubuntu-instance/security.tf
@@ -0,0 +1,25 @@
+resource "oci_core_security_list" "estudo_security_list" {
+    compartment_id = var.compartment_id
+    vcn_id         = oci_core_virtual_network.rede-estudo.id
+    display_name   = "estudoSecurityList"
+
+    egress_security_rules {
+        protocol    = "all"
+        destination = "0.0.0.0/0"
+        }
+
+    ingress_security_rules {
+        protocol = "6"        
+        source   = "0.0.0.0/0"
+
+    tcp_options {
+            max = "22"
+            min = "22"
+        }
+    }
+
+    ingress_security_rules {
+        protocol = "1"
+        source   = "0.0.0.0/0"
+    }
+}
