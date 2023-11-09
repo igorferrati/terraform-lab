5b399412835a801f06432fb66698331cc740f99c
igorferrati <ferrati.igor@gmail.com>
2023-11-09 11:30:40 -0300

    update route tables

--- oci/ubuntu-instance/network.tf
+++ oci/ubuntu-instance/network.tf
@@ -3,7 +3,7 @@ resource "oci_core_vcn" "rede-estudo" {
     compartment_id = var.compartment_id
     display_name   = var.vcn_name
     cidr_block     = var.cidr_block
-    dns_label = "rede-estudo"
+    # dns_label = "rede_estudo"
 }
 
 #subnet
@@ -12,10 +12,10 @@ resource "oci_core_subnet" "public" {
     compartment_id = var.compartment_id
     vcn_id = oci_core_vcn.rede-estudo.id
     display_name = var.public_subnet_name
-    dns_label = "sub-rede-estudo"
+    # dns_label = "sub_rede_estudo"
     freeform_tags = {"Subnet"= "Public"}
     security_list_ids = [oci_core_security_list.estudo_security_list.id]
-    route_table_id = oci_core_vcn.rede-estudo.default_route_table_id
+    route_table_id = oci_core_route_table.estudo_route_table.id
     dhcp_options_id = oci_core_vcn.rede-estudo.default_dhcp_options_id
 }
 
@@ -29,7 +29,7 @@ resource "oci_core_internet_gateway" "test_internet_gateway" {
 #route table
 resource "oci_core_route_table" "estudo_route_table" {
     compartment_id = var.compartment_id
-    display_name = "estudo-route-table"
+    display_name = "estudo_route_table"
     vcn_id = oci_core_vcn.rede-estudo.id
 
     route_rules {
